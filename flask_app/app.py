48669da1bd43b644df0c5c02b0d66471f4c32e00
story <lsy1538756437@126.com>
2024-10-07 22:45:42 -0400

    Add School Comparison

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, session, redirect, url_for
 import os
 import threading
 import pandas as pd
 from visualizations import plot_layered_crime_map, plot_crime_heatmap, plot_trend_analysis, plot_crime_distribution, trend_analysis_over_time, plot_top_universities_crime_info, plot_crime_distribution_top, plot_crime_by_top_university, clean_coordinates
 from crime_scrap import scrape_spotcrime
 from global_vars import stop_scraping_event, current_lat_lon
+from get_dataframe import get_dataframe, create_bar_chart
 
 
 app = Flask(__name__)
 scrape_thread = None
+app.secret_key = 'PythonProject'
 
 # Define paths to your datasets
 file_paths = [
@@ -146,6 +148,40 @@ def scrape_spotcrime_route():
 
     # Inform the user that scraping has started
     return render_template('scrape_status.html', message="SpotCrime data scraping has started.")
+
+@app.route('/compare_school')
+def compare_school():
+    return render_template('compare_school.html')
+
+@app.route('/compare_school_after', methods=['GET', 'POST'])
+def compare_school_after():
+    if request.method == 'POST':
+        school1_name = request.form.get('school1').lower()
+        school2_name = request.form.get('school2').lower()
+        df = get_dataframe()
+
+        # Search for the two schools in the merged dataframe
+        school1 = next((school for index, school in df.iterrows() if school1_name in school['INSTNM'].lower()), None)
+        school2 = next((school for index, school in df.iterrows() if school2_name in school['INSTNM'].lower()), None)
+
+        # Render template with both school results
+        if school1.any() and school2.any():
+            session['school1'] = school1.to_dict()
+            session['school2'] = school2.to_dict()
+            return render_template('compare_school_after.html', school1=school1, school2=school2)
+        else:
+            error_msg = "One or both schools were not found. Please try again."
+            return render_template('`compare_school_after.html', error=error_msg)
+    else:
+        year = request.args.get('year')      
+        school1 = session.get('school1')
+        school2 = session.get('school2')
+
+        if school1 and school2:
+            image_path = create_bar_chart(school1, school2, year)
+            return render_template('compare_school_after.html', school1=school1, school2=school2, year=year, image_file=image_path)
+        else:
+            return redirect(url_for('/home'))
 
 
 @app.route('/scrape_spotcrime_top')
---
+++ flask_app/get_dataframe.py
@@ -0,0 +1,61 @@
+# 读取 ./Crime2023EXCEL 中所有以 filtered_geocoded 开头的csv文件
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+import glob
+
+def merge_csv_files(file_paths):
+    # Initialize an empty DataFrame
+    merged_df = pd.DataFrame()
+    
+    # Iterate over each file in file_paths
+    for file in file_paths:
+        # Read each CSV file into a DataFrame, read first line as column names
+        df = pd.read_csv(file, header=0)
+        # if df only has one row, skip it
+        if len(df) == 1:
+            
+            continue
+        
+        # Merge the DataFrame based on the columns UNITID_P, INSTNM, OPEID
+        if merged_df.empty:
+            merged_df = df
+        else:
+            # Find common columns except the key columns
+            common_columns = set(merged_df.columns).intersection(df.columns) - {'UNITID_P', 'INSTNM', 'OPEID'}
+            
+            merged_df = pd.merge(merged_df, df, on=['UNITID_P', 'INSTNM', 'OPEID'], how='outer', suffixes=('', '_dup'))
+
+            # For the common columns, fill NaN values in the original column with values from the duplicated column
+            for col in common_columns:
+                merged_df.fillna({col: merged_df.pop(f'{col}_dup')}, inplace=True)
+
+
+    return merged_df
+
+def get_dataframe():
+    files = os.listdir('./Crime2023EXCEL')
+    file_paths = [f'./Crime2023EXCEL/{file}' for file in files if file.startswith('filtered_geocoded')]
+
+    merged_df = merge_csv_files(file_paths)
+    return merged_df
+
+def create_bar_chart(school1, school2, year):
+    matplotlib.use('Agg')
+    data = {key[:-2]: [school1[key], school2[key]] for key in school1 if year in key}
+
+    df = pd.DataFrame(data, index=[school1['INSTNM'], school2['INSTNM']])
+
+    ax = df.T.plot(kind='bar', figsize=(10, 6))
+    plt.title(f'Comparison for Year 20{year} between {school1["INSTNM"]} and {school2["INSTNM"]}')
+    plt.ylabel('Number of Incidents')
+    plt.xlabel('Incident Type')
+
+    image_path = os.path.join('./flask_app/static', f'bar_chart.png')
+    plt.xticks(rotation=45, ha="right")
+    plt.tight_layout()
+    plt.savefig(image_path)
+    plt.close()
+
+    return f'bar_chart.png'
